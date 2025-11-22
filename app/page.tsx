27ba5de4e6fe7cf105e63bdb82fe451df04438a2
Ahmed Nasser <nasserahmed093@gmail.com>
2025-11-22 19:53:21 +0200

    Implement Clerk authentication with sign-in component and middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import ClerkSignIn from "@/components/clerkSignIn";
+
 export default function Home() {
   return (
-    <div>
-      <h2>Hello again and again and again</h2>
+    <div className="flex h-screen flex-col items-center justify-center lg:flex-row lg:justify-between lg:w-3/4 lg:mx-auto gap-8 ">
+      <div className="text-center">
+        <h2 className="font-extrabold capitalize text-2xl ">
+          your time perfectely planned
+        </h2>
+        <p className="text-gray-400 text-sm mt-2">
+          Join millions of professionals who easily book meetings with the 1#
+          scheduling tool
+        </p>
+      </div>
+      <ClerkSignIn />
     </div>
   );
 }
