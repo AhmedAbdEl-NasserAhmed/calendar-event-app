48f1b85a1faf295d3dcb12a89ae6b6ece1bd9441
Ahmed Nasser <nasserahmed093@gmail.com>
2025-11-22 20:25:06 +0200

    Add sign-in and sign-up pages with Clerk integration and update layout styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 import ClerkSignIn from "@/components/clerkSignIn";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex h-screen flex-col items-center justify-center lg:flex-row lg:justify-between lg:w-3/4 lg:mx-auto gap-8 ">
       <div className="text-center">
-        <h2 className="font-extrabold capitalize text-2xl ">
-          your time perfectely planned
+        <h2 className="font-extrabold capitalize text-3xl ">
+          your time is perfectely planned
         </h2>
+        <Image
+          className="hidden lg:block mx-auto"
+          src="/assets/planning.svg"
+          alt="planning"
+          width={400}
+          height={400}
+        />
         <p className="text-gray-400 text-sm mt-2">
           Join millions of professionals who easily book meetings with the 1#
           scheduling tool
--- components/clerkSignIn.tsx
+++ components/clerkSignIn.tsx
@@ -1,7 +1,8 @@
+"use client";
 import { SignIn } from "@clerk/nextjs";
 
 const ClerkSignIn = () => {
-  return <SignIn />;
+  return <SignIn routing="hash" />;
 };
 
 export default ClerkSignIn;
