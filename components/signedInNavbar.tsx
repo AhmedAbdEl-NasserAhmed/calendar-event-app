6018f8daf161a63ea99dd629fbe1b6d0674774f7
Ahmed Nasser <nasserahmed093@gmail.com>
2025-11-23 17:12:19 +0200

    Refactor SignedInNavbar: adjust padding and image sizes for better layout

--- components/signedInNavbar.tsx
+++ components/signedInNavbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const SignedInNavbar = () => {
   return (
-    <nav className="flex justify-between  p-8 bg-blue-300/50 drop-shadow-2xl">
+    <nav className="flex justify-between  px-8 py-6 bg-blue-300/50 drop-shadow-2xl">
       <div>
         <Link href="/">
           <Image src="/assets/logo.svg" alt="planning" width={80} height={80} />
@@ -16,8 +16,8 @@ const SignedInNavbar = () => {
             <Image
               src="/assets/events.svg"
               alt="planning"
-              width={80}
-              height={80}
+              width={65}
+              height={65}
             />
             <h2 className="font-bold">My Events</h2>
           </Link>
@@ -27,8 +27,8 @@ const SignedInNavbar = () => {
             <Image
               src="/assets/schedule.svg"
               alt="Schedule"
-              width={80}
-              height={80}
+              width={65}
+              height={65}
             />
             <h2 className="font-bold">My Schedule</h2>
           </Link>
@@ -38,13 +38,14 @@ const SignedInNavbar = () => {
             <Image
               src="/assets/public.svg"
               alt="Public Profile"
-              width={80}
-              height={80}
+              width={65}
+              height={65}
             />
             <h2 className="font-bold">Public Profile</h2>
           </Link>
         </li>
       </ul>
+
       <ClerkUserButton />
     </nav>
   );
